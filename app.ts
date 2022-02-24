29fba72072f9c791576522f786e9647aae61390f
Dan McAtee <danielmcatee@me.com>
2022-02-24 11:57:41 +0100

    refactor player placement to PlayerGrid class

--- app.ts
+++ app.ts
@@ -11,89 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
   playerGrid.createBoard();
   computerGrid.createBoard();
 
-  // Create all five ships and put them into an array
-  const shipsArray = shipNames.map((shipName) => new PlayerShip(shipName));
-
   // when rotate button is clicked rotate every ship
   rotateButton.addEventListener("click", () =>
-    shipsArray.forEach((ship) => ship.rotate())
+    playerGrid.shipsToBePlaced.forEach((ship) => ship.rotate())
   );
 
-  // the part of the ship we click on before we start dragging
-  let selectedShipPart: number;
-  // the whole instance of a ship
-  let selectedShip: PlayerShip;
-
-  shipsArray.forEach((ship) => {
-    ship.element.addEventListener("mousedown", (event) => {
-      const target = event.target as HTMLElement;
-      // take e.g. destroyer-0 and assign the number after the "-" to selectedShipPart
-      selectedShipPart = parseInt(target.id.split("-")[1]);
-    });
-    ship.element.addEventListener("dragstart", () => {
-      selectedShip = ship;
-    });
-  });
-
-  playerGrid.element.addEventListener("dragstart", (event) =>
-    event.preventDefault()
-  );
-  playerGrid.element.addEventListener("dragover", (event) =>
-    event.preventDefault()
-  );
-  playerGrid.element.addEventListener("dragenter", (event) =>
-    event.preventDefault()
-  );
-  playerGrid.element.addEventListener("dragleave", (event) =>
-    event.preventDefault()
-  );
-  playerGrid.element.addEventListener("drop", (event) => {
-    const target = event.target as HTMLElement;
-    const position = makePositionFromId(target.id);
-    placeShip(selectedShip, selectedShipPart, position);
-  });
-
-  function placeShip(ship: PlayerShip, shipPart: number, position: Position) {
-    // Create a binding that holds all of the positions the ship takes
-    const shipSquares: Position[] = [];
-    // "a-1" -> "a"
-    const positionChar = position.split("-")[0];
-    // "a-1" -> 1
-    const positionNumber = parseInt(position.split("-")[1]);
-
-    if (ship.isHorizontal) {
-      // Determine how far the ship extends horizontally (number)
-      for (let i = 0; i < ship.length; i++) {
-        // calculate the position that each part of the ship would take
-        const number = positionNumber + i - shipPart;
-        // if ship occupies a cell with a number greater than 10 or less than 1 -> invalid
-        if (number > 10 || number < 1) {
-          // Return ship if position is invalid
-          return;
-        }
-        shipSquares.push(`${positionChar}-${number}`);
-      }
-      console.log(shipSquares);
-    } else {
-      for (let i = 0; i < ship.length; i++) {
-        const charIndex = gridChars.indexOf(positionChar);
-        // calculate the position that each part of the ship would take
-        const char = gridChars[charIndex + i - shipPart];
-        if (!char) {
-          return;
-        }
-        shipSquares.push(`${char}-${positionNumber}`);
-      }
-    }
-
-    const isTaken: boolean = shipSquares.some((square) =>
-      playerGrid.get(square)
-    );
-
-    if (!isTaken) {
-      shipSquares.forEach((square) => playerGrid.set(square, ship.type));
-      // drawShip
-      ship.element.remove();
-    }
-  }
+  playerGrid.addListeners();
 });
--- grid.ts
+++ grid.ts
@@ -3,7 +3,7 @@ type PossibleValue = "" | ShipType;
 type GridState = Record<Position, PossibleValue>;
 
 abstract class Grid {
-  protected state: GridState;
+  protected state: GridState = {};
   type: "player" | "computer";
   ships: Ship[] = [];
   element: HTMLElement;
@@ -11,7 +11,6 @@ abstract class Grid {
 
   constructor(type: "player" | "computer") {
     this.type = type;
-    this.state = {};
 
     for (let i = 0; i < gridChars.length; i++) {
       for (let j = 1; j <= 10; j++) {
@@ -61,11 +60,106 @@ abstract class Grid {
     const value = this.get(position);
     return Boolean(value);
   }
+
+  drawShip(positions: Position[], shipType: ShipType): void {
+    positions.forEach((position) => {
+      const square = document.getElementById(`${this.type}-${position}`);
+      square?.classList.add(shipType);
+    });
+  }
+
+  isTaken(positions: Position[]): boolean {
+    return positions.some((square) => this.get(square));
+  }
 }
 
 class PlayerGrid extends Grid {
+  shipsToBePlaced: PlayerShip[] = [];
+  selectedShipPart: number = 0;
+  selectedShip: PlayerShip | null = null;
+
   constructor() {
     super("player");
+
+    shipNames.forEach((shipName) =>
+      this.shipsToBePlaced.push(new PlayerShip(shipName))
+    );
+  }
+
+  addListeners(): void {
+    this.shipsToBePlaced.forEach((ship) => {
+      ship.element.addEventListener("mousedown", (event) => {
+        const target = event.target as HTMLElement;
+        // take e.g. destroyer-0 and assign the number after the "-" to selectedShipPart
+        this.selectedShipPart = parseInt(
+          target.id.substring(target.id.length - 1)
+        );
+      });
+      ship.element.addEventListener("dragstart", () => {
+        this.selectedShip = ship;
+      });
+    });
+
+    this.element.addEventListener("dragstart", (event) =>
+      event.preventDefault()
+    );
+    this.element.addEventListener("dragover", (event) =>
+      event.preventDefault()
+    );
+    this.element.addEventListener("dragenter", (event) =>
+      event.preventDefault()
+    );
+    this.element.addEventListener("dragleave", (event) =>
+      event.preventDefault()
+    );
+    this.element.addEventListener("drop", (event) => {
+      const target = event.target as HTMLElement;
+      const position = makePositionFromId(target.id);
+      if (this.selectedShip) {
+        this.placeShip(this.selectedShip, this.selectedShipPart, position);
+      }
+    });
+  }
+
+  placeShip(ship: PlayerShip, shipPart: number, position: Position) {
+    // Create a binding that holds all of the positions the ship takes
+    const shipSquares: Position[] = [];
+    // "a-1" -> "a"
+    const positionChar = position.split("-")[0];
+    // "a-1" -> 1
+    const positionNumber = parseInt(position.split("-")[1]);
+
+    if (ship.isHorizontal) {
+      // Determine how far the ship extends horizontally (number)
+      for (let i = 0; i < ship.length; i++) {
+        // calculate the position that each part of the ship would take
+        const number = positionNumber + i - shipPart;
+        // if ship occupies a cell with a number greater than 10 or less than 1 -> invalid
+        if (number > 10 || number < 1) {
+          // Return ship if position is invalid
+          return;
+        }
+        shipSquares.push(`${positionChar}-${number}`);
+      }
+    } else {
+      for (let i = 0; i < ship.length; i++) {
+        const charIndex = gridChars.indexOf(positionChar);
+        // calculate the position that each part of the ship would take
+        const char = gridChars[charIndex + i - shipPart];
+        if (!char) {
+          return;
+        }
+        shipSquares.push(`${char}-${positionNumber}`);
+      }
+    }
+
+    const isTaken = this.isTaken(shipSquares);
+
+    if (!isTaken) {
+      shipSquares.forEach((square) => this.set(square, ship.type));
+      this.drawShip(shipSquares, ship.type);
+      ship.element.remove();
+    }
   }
 }
 
